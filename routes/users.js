39d629c37ec277e08a16e5d10f02f0e5bb0a541f
Michael Gatewood <michael@coincloudnv.com>
2024-11-15 15:52:32 -0800

    Add user routes

---
+++ routes/users.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import pool from '../db.js';
+import bcrypt from 'bcrypt';
+import { authToken } from '../middleware/authorization.js';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+	try {
+		const { username, email, password } = req.body;
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		const response = await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *', [username, email, hashedPassword]);
+		res.json({ data: { user: response.rows[0] } });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+router.post('/favorites', authToken, async (req, res) => {
+	try {
+		const { user_id, movie_id } = req.body;
+		const response = await pool.query('INSERT INTO favorites (user_id, movie_id) VALUES ($1, $2) RETURNING *', [user_id, movie_id]);
+		res.json({ data: { favorite: response.rows[0] } });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+router.get('/:user_id/favorites', authToken, async (req, res) => {
+	try {
+		const userId = req.params.user_id;
+		const response = await pool.query('SELECT * FROM favorites WHERE user_id = $1', [userId]);
+		res.json({ data: { user: { id: userId }, favorites: response.rows } });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+router.delete('/favorites', authToken, async (req, res) => {
+	try {
+		const { user_id, movie_id } = req.body;
+		const response = await pool.query('DELETE FROM favorites WHERE user_id = $1 AND movie_id = $2 RETURNING *', [user_id, movie_id]);
+		res.status(200).json({ message: 'Favorite deleted' });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+export default router;
